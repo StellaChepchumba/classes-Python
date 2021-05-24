f8c7e785124ae73b91829bd4c8ee2d766c036eac
stella4545 <stellachepchumbah@gmail.com>
2021-05-24 15:05:10 +0300

    second commit

--- student.py
+++ student.py
@@ -1,4 +1,12 @@
-
+class Student:
+    school:"AkiraChix"
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def details(self):
+            return(f"my name is {self.name} and iam {self.age} years")
+    def fullName(self):
+            return (f"my full name is {self.name}")
     
 
    
