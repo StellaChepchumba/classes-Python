e1c730f6bef8dd1490d505a289bb0d03dc9e1472
stella4545 <stellachepchumbah@gmail.com>
2021-05-25 00:10:13 +0300

    second commit

--- bank.py
+++ bank.py
@@ -1,10 +1,32 @@
 class BankAccount:
-    name="equity"
-    def __init__(self,branch,location,Account):
-        self.branch=branch
-        self.location=location
-        self.Account=Account
-    def message(self):
-        return (f"This is equity bank ,{self.branch} ,{self.location} location  and your Bank account is {self.Account}")
-    def datails(self):
-        return ("This is {self.branch} branch")
+    
+    def __init__(self,name,phoneNumber,loan):
+        self.phoneNuber=phoneNumber
+        self.balance=0
+        self.name=name
+        self.loan=loan
+    def amount(self):
+        return f"your amount is {self.balance}"
+    def name(self):
+        return f"{self.name} is a good bank"
+
+    def showBalance(self):
+        return f"Hello {self.name} ,your balance is {self.balance}"
+    def deposit(self,amount):
+        if amount<0:
+            return f"hello {self.name} your balance is {self.balance}"
+        else:
+            self.balance+=amount
+            return self.showBalance
+
+    def withdraw(self,amount):
+        if amount > self.balance:
+            return "your balance is {self.balance} you cannot withdraw {amount}"
+        else:
+                self.balance==amount
+                return self.showBalance()
+    def addLoan(self,amount):
+        return f"hello {self.name},you can add a loan of {amount-self.loan}"
+    def payloan(self,amount):
+        return f"you have a loan due of ksh {amount}"
+               
--- student.py
+++ student.py
@@ -1,5 +1,5 @@
 class Student:
-    school:"AkiraChix"
+    school="AkiraChix"
     def __init__(self,name,age):
         self.name=name
         self.age=age
@@ -7,12 +7,17 @@ class Student:
             return(f"my name is {self.name} and iam {self.age} years")
     def fullName(self):
             return (f"my full name is {self.name}")
-    
-
-   
-   
-   
-   
+    def __init__(self,first_name,last_name,age) :
+        self.first_name=first_name
+        self.last_name=last_name
+        self.age=age
+    def greet(self,name):
+        return f"hello{self.first_name},welcome to {self.school}"
+    def year_of_birth(self):
+        return f"Hello {self.first_name},you were born in {2021-self.age}"
+    def initialise(self):
+        return f"your initials are {self.first_name[0]} {self.last_name}"
+        
    
    
    
