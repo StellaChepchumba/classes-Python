c936661600f244575d0f244d87036be94aab6eed
stella4545 <stellachepchumbah@gmail.com>
2021-06-14 21:31:28 +0300

    fourth commit

--- bank.py
+++ bank.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 class BankAccount:
     
-    def __init__(self,name,phoneNumber,loan):
+    def __init__(self,name,phoneNumber,):
         self.phoneNuber=phoneNumber
         self.balance=0
         self.account=name
-        self.loan=loan  
+     
         self.statement=[]
     
     def amount(self):
@@ -16,6 +16,11 @@ class BankAccount:
     def showBalance(self):
         return f"Hello {self.name} ,your balance is {self.balance}"
     def deposit(self,amount):
+        try:
+            10+amount
+        except TypeError:
+                    return f"The amount must be string"
+           
         if amount<0:
             return f"hello {self.name} your balance is {self.balance}"
         else:
@@ -27,9 +32,16 @@ class BankAccount:
                 "narration":"you deposited"
             }
             self.statement.append(transaction)
+        
+            
+
            
     
     def withdraw(self,amount):
+        try:
+            10+amount
+        except TypeError:
+                    return f"The amount must be string"
         if amount > self.balance:
             return f"your balance is {self.balance} you cannot withdraw {amount}"
         else:
@@ -46,6 +58,10 @@ class BankAccount:
     def addLoan(self,amount):
         return f"hello {self.name},you can add a loan of {amount-self.loan}"
     def payloan(self,amount):
+        try:
+            10+amount
+        except TypeError:
+                    return f"The amount must be string"
         return f"you have a loan due of ksh {amount}"
     def show_statement(self):
         for transaction in self.statement:
@@ -56,6 +72,10 @@ class BankAccount:
             print(f"{date}:{narration} {amount}")
         return 
     def borrow(self,amount):
+        try:
+            10+amount
+        except TypeError:
+                    return f"The amount must be string"
         if amount<0:
             return "Your amount is too low"
         elif self.loan>0:
@@ -76,6 +96,10 @@ class BankAccount:
             return "you have successfully borrowed loan"
 
     def repayloan(self,amount):
+        try:
+            10+amount
+        except TypeError:
+                    return f"The amount must be string"
         if amount<0:
             return "you cannot repay with that"
         elif amount<self.loan:
@@ -93,7 +117,41 @@ class BankAccount:
             }
             self.statement.append(transaction)
             return "you have successfully paid your loan"
+    def transfer(self,BankAccount,amount):
+        self.BankAccount=BankAccount
+        self.amount=amount
+        transactionfee=0.05*amount
+        total=transactionfee+amount
+        try:
+            10+amount
+        except TypeError:
+                    return f"The amount must be in figures"
+    
+        if total>self.balance:
+            return f"your balance is {self.balance} and you need at least {total} for this transfer"
+        else:
+            self.balance-=total
+            BankAccount.deposit(amount)
 
+class MobileMoneyAccount(BankAccount):
+    def  __init__(self,name,phoneNumber,service_provider):
+        BankAccount .__init__ (name,phoneNumber)
+        self.service_provider=service_provider
+
+    def buy_airtime(self,amount):
+        try:
+            10+amount
+        except TypeError:
+            return f"The amount must be in figures"
+        if amount<0:
+            return f"your amount is low to purchase this airtime"
+        elif self.balance<amount:
+            return f"your balance is low to purchase this amount of credit"
+        else:
+            self.balance-=amount
+            return f"you have purchased airtime worth {amount}from {self.service_provider}.Your account balance is {self.balance}"
+
+    
 
            
 
