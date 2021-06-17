79341c1386278d343a1b958a2ab01a307d411670
stella4545 <stellachepchumbah@gmail.com>
2021-06-17 12:05:44 +0300

    changes

---
+++ pyquiz/python_test.py
@@ -0,0 +1,44 @@
+x = [100,110,120,130,140,150]
+y=[]
+for a in x:
+    a*5
+    x.append(y)
+
+    def divisible_by_three(n):
+        n=50
+        if(n%3==0):
+            print(n)
+
+x = [[1,2],[3,4],[5,6]]
+s=[[1,2]+[3,4]+[5,6]]
+print(s)
+
+smallest=([12,45,2,4,2,9,])
+
+x = ['a','b','a','e','d','b','c','e','f','g','h']
+y=set(x)
+print(y)
+
+def divisible_by_seven():
+    divide=range(100,200)
+    for a in divide:
+        if a%7==0:
+            print(a)
+
+
+students = [{"age": 19, "name": "Eunice"}, {"age": 21, "name": "Agnes"}, {"age": 18, "name": "Teresa"}, {"age": 22, "name": "Asha"}]
+for student in students:
+    student.values
+    print("Hello {student.name} , you were born in {2021-student.age}")
+
+
+
+class Rectangle:
+    def __init__(self,width,length):
+        self.width=width
+        self.length=length
+    def area(self,width,length):  
+        A=width*length 
+
+    def perimeter(self,width,length):
+       p=2(length+width),
